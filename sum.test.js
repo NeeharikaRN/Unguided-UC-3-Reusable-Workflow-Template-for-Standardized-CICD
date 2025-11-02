59ada037b39ee69a4e5e17e4cefdb57a36d49ca5
NeeharikaRN <n.neeharikareddy@gmail.com>
2025-11-02 22:03:44 +0530

    Create sum.test.js

---
+++ sum.test.js
@@ -0,0 +1,8 @@
+import { sum } from './sum.js';
+
+test('adds two numbers correctly', () => {
+  expect(sum(3, 4)).toBe(7);
+});
+
+test('throws error if inputs are not numbers', () => {
+  expect(() => sum('a', 5)).toThrow('Inputs must be numbers');
